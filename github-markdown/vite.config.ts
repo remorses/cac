f742fb81cfca540d27f529b171b17ef10d82afdd
remorses <beats.by.morse@gmail.com>
2024-07-27 17:02:09 +0200

    added template plugin

---
+++ github-markdown/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import EnvironmentPlugin from 'vite-plugin-environment'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
+import framer from 'vite-plugin-framer'
+
+const building = process.env.NODE_ENV === 'production'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        mkcert(),
+        framer(),
+        EnvironmentPlugin('all', { prefix: 'PUBLIC' }),
+        tsconfigPaths(),
+    ],
+})
