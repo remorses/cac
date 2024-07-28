1022b581ce9b6905c33b99d19a659c030ac15c68
remorses <beats.by.morse@gmail.com>
2024-07-28 21:52:15 +0200

    website/src/lib/markdown-plugin.test: moretest

--- website/src/lib/markdown-plugin.test.ts
+++ website/src/lib/markdown-plugin.test.ts
@@ -54,6 +54,30 @@ This is an example markdown file.
 # This should not be a title
 
 ## another h2
+
+# @elysiajs/eden
+Fully type-safe Elysia client refers to the [documentation](https://elysiajs.com/eden/overview)
+
+
+## Example
+\`\`\`typescript
+// server.ts
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia()
+    .get('/', () => 'Hi Elysia')
+    .get('/id/:id', ({ params: { id } }) => id)
+    .post('/mirror', ({ body }) => body, {
+        schema: {
+            body: t.Object({
+                id: t.Number(),
+                name: t.String()
+            })
+        }
+    })
+    .listen(8080)
+
+\`\`\`
 `
 
 const owner = 'x'
@@ -92,6 +116,25 @@ test('processMarkdown', async () => {
       </blockquote>
       <h1>This should not be a title</h1>
       <h2>another h2</h2>
+      <h1>@elysiajs/eden</h1>
+      <p>Fully type-safe Elysia client refers to the <a href="https://elysiajs.com/eden/overview">documentation</a></p>
+      <h2>Example</h2>
+      <pre><code class="language-typescript">// server.ts
+      import { Elysia, t } from &#39;elysia&#39;
+
+      const app = new Elysia()
+          .get(&#39;/&#39;, () =&gt; &#39;Hi Elysia&#39;)
+          .get(&#39;/id/:id&#39;, ({ params: { id } }) =&gt; id)
+          .post(&#39;/mirror&#39;, ({ body }) =&gt; body, {
+              schema: {
+                  body: t.Object({
+                      id: t.Number(),
+                      name: t.String()
+                  })
+              }
+          })
+          .listen(8080)
+      </code></pre>
       ",
         "pagePath": "/example",
         "slug": "/example",
