55e6c4044efb092cafbf05189e69365da94a943f
remorses <beats.by.morse@gmail.com>
2024-10-02 18:38:33 +0200

    works, get all creators

--- website/scripts/get-creators-emails.ts
+++ website/scripts/get-creators-emails.ts
@@ -1,4 +1,5 @@
 import { getSitemapLinks } from './sitemap'
+import { Sema } from 'sema4'
 import fs from 'fs'
 import * as Papa from 'papaparse'
 
@@ -35,8 +36,8 @@ const allEmails = [] as {
     lastTemplateSubmitted: string
     ctaLink: string
     usesLemonSqueezy: boolean
+    templateName: string
 }[]
-
 async function main() {
     let links = await getSitemapLinks(
         'https://www.framer.com/marketplace/sitemap.xml',
@@ -44,108 +45,151 @@ async function main() {
     let base = 'https://www.framer.com'
     links = links.filter((x) => x.includes('/template/'))
     console.log(`found ${links.length} templates`)
+    const sema = new Sema(10)
 
-    for (let templateLink of links) {
-        console.log('........................................................')
-        let abortController = new AbortController()
-        console.log(`fetching ${templateLink}`)
-        const res = await retryFetchTwice(templateLink, {
-            signal: abortController.signal,
-            headers: {
-                accept: 'text/html',
-            },
-        })
-        if (!res.ok) {
-            console.log('not ok', res.status)
-            continue
-        }
+    await Promise.all(
+        links.map(async (templateLink) => {
+            await sema.acquire()
+            try {
+                console.log(
+                    '........................................................',
+                )
+                let abortController = new AbortController()
+                console.log(`fetching ${templateLink}`)
+                const res = await retryFetchTwice(templateLink, {
+                    signal: abortController.signal,
+                    headers: {
+                        accept: 'text/html',
+                    },
+                })
+                if (!res.ok) {
+                    console.log('not ok', res.status)
+                    return
+                }
 
-        let creatorName = ''
-        let twitter = ''
-        let emailLink = ''
-        let lastTemplateSubmitted = ''
-        let usesLemonSqueezy = false
-        let ctaLink = ''
-        let lastText = ''
+                let creatorName = ''
+                let twitter = ''
+                let emailLink = ''
+                let lastTemplateSubmitted = ''
+                let usesLemonSqueezy = false
+                let ctaLink = ''
+                let lastText = ''
+                let templateName = ''
 
-        await new HTMLRewriter()
-            .on('h1', {
-                text(e) {
-                    creatorName += e.text
-                },
-            })
-            .on('a', {
-                element(e) {
-                    const href = e.getAttribute('href')
-                    if (!href) {
-                        return
-                    }
+                await new HTMLRewriter()
+                    .on('h1', {
+                        text(e) {
+                            templateName += e.text
+                        },
+                    })
+                    .on('a', {
+                        element(e) {
+                            const href = e.getAttribute('href')
+                            if (!href) {
+                                return
+                            }
+                            if (
+                                !href.includes('/framer') &&
+                                (href.includes('x.com') ||
+                                    href.includes('twitter.com'))
+                            ) {
+                                twitter = href
+                            }
+                            if (
+                                href.includes('mailto:') &&
+                                !href.includes('@framer.com')
+                            ) {
+                                emailLink = extractEmailFromLink(href)
+                            }
+                            if (href.includes('lemonsqueezy.com')) {
+                                usesLemonSqueezy = true
+                            }
+                            const className = e.getAttribute('class')
+                            if (
+                                className &&
+                                className.includes('btn-primary') &&
+                                e.getAttribute('target') === '_blank'
+                            ) {
+                                ctaLink = href
+                            }
+                        },
+                    })
+                    .on('p', {
+                        text(chunk) {
+                            if (!chunk.text.trim()) {
+                                return
+                            }
+                            if (chunk.text && lastText.includes('Published')) {
+                                try {
+                                    console.log(`parsing ${chunk.text}`)
+                                    let parsed = Date.parse(
+                                        chunk.text
+                                            .replace('Published ', '')
+                                            .trim(),
+                                    )
+                                    lastTemplateSubmitted = new Date(
+                                        parsed,
+                                    ).toISOString()
+                                } catch (e) {
+                                    console.log(
+                                        'error parsing date',
+                                        chunk.text,
+                                    )
+                                }
+                            }
+                            lastText = chunk.text
+                        },
+                    })
+                    .transform(res)
+                    .text()
+                    .catch(ignoreAbortError)
+
+                if (!emailLink) {
+                    console.log('no email found for', creatorName)
+                    return
+                }
+
+                const existingEmailIndex = allEmails.findIndex(
+                    (item) => item.email === emailLink,
+                )
+                if (existingEmailIndex === -1) {
+                    allEmails.push({
+                        email: emailLink,
+                        creatorName: creatorName,
+                        exampleTemplate: templateLink,
+                        twitter,
+                        lastTemplateSubmitted,
+                        ctaLink,
+                        usesLemonSqueezy,
+                        templateName,
+                    })
+                    console.log('Last:', allEmails[allEmails.length - 1])
+                } else {
+                    console.log('Creator already processed:', emailLink)
+                    // Update the existing entry if needed
                     if (
-                        !href.includes('/framer') &&
-                        (href.includes('x.com') || href.includes('twitter.com'))
+                        lastTemplateSubmitted &&
+                        (!allEmails[existingEmailIndex].lastTemplateSubmitted ||
+                            new Date(lastTemplateSubmitted) >
+                                new Date(
+                                    allEmails[
+                                        existingEmailIndex
+                                    ].lastTemplateSubmitted,
+                                ))
                     ) {
-                        twitter = href
+                        allEmails[existingEmailIndex].lastTemplateSubmitted =
+                            lastTemplateSubmitted
                     }
-                    if (
-                        href.includes('mailto:') &&
-                        !href.includes('@framer.com')
-                    ) {
-                        emailLink = extractEmailFromLink(href)
-                    }
-                    if (href.includes('lemonsqueezy.com')) {
-                        usesLemonSqueezy = true
-                    }
-                    const className = e.getAttribute('class')
-                    if (
-                        className &&
-                        className.includes('btn-primary') &&
-                        e.getAttribute('target') === '_blank'
-                    ) {
-                        ctaLink = href
-                    }
-                },
-            })
-            .on('p', {
-                text(chunk) {
-                    if (!chunk.text.trim()) {
-                        return
-                    }
-                    if (chunk.text && lastText.includes('Published')) {
-                        try {
-                            console.log(`parsing ${chunk.text}`)
-                            let parsed = Date.parse(
-                                chunk.text.replace('Published ', '').trim(),
-                            )
-                            lastTemplateSubmitted = new Date(
-                                parsed,
-                            ).toISOString()
-                        } catch (e) {
-                            console.log('error parsing date', chunk.text)
-                        }
-                    }
-                    lastText = chunk.text
-                },
-            })
-            .transform(res)
-            .text()
-            .catch(ignoreAbortError)
+                    allEmails[existingEmailIndex].usesLemonSqueezy =
+                        usesLemonSqueezy ||
+                        allEmails[existingEmailIndex].usesLemonSqueezy
+                }
+            } finally {
+                sema.release()
+            }
+        }),
+    )
 
-        if (!emailLink) {
-            console.log('no email found for', creatorName)
-            continue
-        }
-
-        allEmails.push({
-            email: emailLink,
-            creatorName: creatorName,
-            exampleTemplate: templateLink,
-            twitter,
-            lastTemplateSubmitted,
-            ctaLink,
-            usesLemonSqueezy,
-        })
-        console.log('Last:', allEmails[allEmails.length - 1])
-    }
     return allEmails
 }
 
