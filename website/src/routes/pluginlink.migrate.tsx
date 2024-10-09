720924edd67cdc9515278838ce9b3a89edc948af
remorses <beats.by.morse@gmail.com>
2024-10-09 12:53:25 +0200

    add sleep

--- website/src/routes/pluginlink.migrate.tsx
+++ website/src/routes/pluginlink.migrate.tsx
@@ -1,10 +1,13 @@
 import { Spinner } from '@nextui-org/react'
 import { useEffect } from 'react'
+import { sleep } from 'website/src/lib/utils'
 
 export default function PluginLinkMigrate() {
     useEffect(() => {
-        window.location.href =
-            'https://www.framer.com/marketplace/plugins/migrate--atog5qz8imo0pji1b10z8alr7/'
+        sleep(2 * 1000).then(() => {
+            window.location.href =
+                'https://www.framer.com/marketplace/plugins/migrate--atog5qz8imo0pji1b10z8alr7/'
+        })
     }, [])
 
     return (
@@ -17,6 +20,7 @@ export default function PluginLinkMigrate() {
                     __html: 'window.lemonSqueezyAffiliateConfig = { store: "unframer", debug: true };',
                 }}
             />
+
             <script src='https://lmsqueezy.com/affiliate.js' />
         </>
     )
