98e1eace749454511f69e3d900a479219eeeccb2
remorses <beats.by.morse@gmail.com>
2024-10-09 12:49:55 +0200

    add pluginlink page

---
+++ website/src/routes/pluginlink.migrate.tsx
@@ -0,0 +1,23 @@
+import { Spinner } from '@nextui-org/react'
+import { useEffect } from 'react'
+
+export default function PluginLinkMigrate() {
+    useEffect(() => {
+        window.location.href =
+            'https://www.framer.com/marketplace/plugins/migrate--atog5qz8imo0pji1b10z8alr7/'
+    }, [])
+
+    return (
+        <>
+            <div className='flex flex-col items-center justify-center h-screen'>
+                <Spinner />
+            </div>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: 'window.lemonSqueezyAffiliateConfig = { store: "unframer", debug: true };',
+                }}
+            />
+            <script src='https://lmsqueezy.com/affiliate.js' />
+        </>
+    )
+}
