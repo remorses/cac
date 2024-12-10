24235e271de04bd6a72199da6fba4ac6bd1654dc
remorses <beats.by.morse@gmail.com>
2024-12-10 20:16:34 +0100

    pg hmr things

--- db/kysely.ts
+++ db/kysely.ts
@@ -2,12 +2,22 @@ import { Kysely, PostgresDialect } from 'kysely'
 import { DB } from './kysely.types'
 import { Pool } from 'pg'
 
+declare global {
+    var __pg_pool: Pool | undefined
+}
+
+const pool = globalThis.__pg_pool || new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 2,
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    globalThis.__pg_pool = pool
+}
+
 export const db = new Kysely<DB>({
     dialect: new PostgresDialect({
-        pool: new Pool({
-            connectionString: process.env.DATABASE_URL,
-            max: 2,
-        }),
+        pool,
     }),
 })
 
