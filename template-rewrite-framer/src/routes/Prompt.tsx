6bc5cd2aa8d6e64abc9d8e7198bf698406355249
remorses <beats.by.morse@gmail.com>
2025-01-12 20:42:50 +0100

    created framer utils file

--- template-rewrite-framer/src/routes/Prompt.tsx
+++ template-rewrite-framer/src/routes/Prompt.tsx
@@ -52,13 +52,16 @@ import {
     oldTextTreeToXml,
     sleep,
 } from 'website/src/lib/utils'
+import {
+    discardFramerChanges,
+    getFramerTree,
+    isNodeZoomable,
+    NodeWithControl,
+} from 'template-rewrite-framer/src/lib/framer'
 
 let abortController = new AbortController()
 
-let instanceNodes = new Map<
-    string,
-    { node: ComponentInstanceNode; controlKey: string }
->()
+let instanceNodes = new Map<string, NodeWithControl>()
 
 function SimplePromptComponent({}) {
     const { buyMoreCreditsUrl, credits } = useLoaderData() as LoaderReturnType<
@@ -158,7 +161,7 @@ function SimplePromptComponent({}) {
             return
         }
 
-        let oldText = await getFramerTree(rootNodes)
+        let oldText = await getFramerTree({ rootNodes, instanceNodes })
         // @ts-ignore
         if (import.meta.env?.DEV) {
             try {
@@ -385,7 +388,7 @@ function SimplePromptComponent({}) {
         setIsDiscarding(true)
         try {
             await Promise.all([
-                discardFramerChanges(previousOldText),
+                discardFramerChanges({ previousOldText, instanceNodes }),
                 pluginApiClient.api.plugins.rewritePlugin.discardGeneration.post(
                     {
                         id: generationId,
@@ -584,303 +587,4 @@ async function loader({}: LoaderFunctionArgs) {
         credits,
         buyMoreCreditsUrl,
     }
-}
-
-async function getInstanceComponent(componentInstance: AnyNode) {
-    if (!isComponentInstanceNode(componentInstance)) {
-        return
-    }
-    // console.log('controls', componentInstance.controls)
-    if (!componentInstance.componentIdentifier.startsWith('local-module:')) {
-        console.log(`component ${componentInstance.name} is not a local module`)
-        return
-    }
-    const regex = /local-module:.*\/(.*):.*/
-    const match = componentInstance.componentIdentifier.match(regex)
-    if (!match) {
-        console.log(
-            `component ${componentInstance.name} does not match regex to get component id`,
-        )
-        return
-    }
-
-    const componentId = match[1]
-    const componentNode = await framer.getNode(componentId)
-    if (!componentNode || !isComponentNode(componentNode)) {
-        console.log(`could not find component node for ${componentId}`)
-        return
-    }
-
-    return componentNode
-}
-
-async function getComponentCodeUrl(componentNode?: AnyNode) {
-    // example is https://framer.com/m/FAQ-Row-Copy-FR9A9RBHB.js
-    // https://framer.com/m/AccordionOne-V8Wz.js@FR9A9RBHB
-    if (isComponentInstanceNode(componentNode)) {
-        return await getComponentCodeUrl(
-            await getInstanceComponent(componentNode),
-        )
-    }
-    if (isComponentNode(componentNode)) {
-        let nameEncoding = componentNode.name || ''
-        if (!nameEncoding) {
-            return
-        }
-
-        // turn FAQ Row Copy into FAQ-Row-Copy, replace space with -
-        nameEncoding = nameEncoding.replace(/ +/g, '-')
-        nameEncoding = encodeURIComponent(nameEncoding)
-        let id = componentNode.id
-        return `https://framer.com/m/${nameEncoding}-${id}.js`
-    }
-    // console.log('not a component node', componentNode?.constructor?.name)
-}
-Object.assign(globalThis, { getComponentCodeUrl })
-
-async function* recurseIntoComponent(
-    componentInstance: AnyNode,
-    encounteredIds: Set<string>,
-) {
-    const componentNode = await getInstanceComponent(componentInstance)
-    if (!componentNode) {
-        return
-    }
-    const primary = (await componentNode.getChildren()).find(
-        (x) => isFrameNode(x) && !x.isReplica,
-    )
-    if (!primary) {
-        console.log('no primary child for component found')
-        return
-    }
-
-    for await (let child of primary.walk()) {
-        if (!encounteredIds.has(child.id)) {
-            encounteredIds.add(child.id)
-            yield child
-            yield* recurseIntoComponent(child, encounteredIds)
-        }
-    }
-    // TODO get modified text nodes in replicas, so there is nothing left that is stale because text is overridden in breakpoint
-}
-
-async function isNodeVisible(node: AnyNode) {
-    const parents = await collectGenerator(getParentNodes(node))
-    const isVisible = parents.every((parent) => {
-        if (supportsVisible(parent)) {
-            return parent.visible
-        }
-        return true
-    })
-    return isVisible && (!supportsVisible(node) || node.visible)
-}
-async function isNodeZoomable(node: AnyNode) {
-    if (!(await isNodeVisible(node))) {
-        return false
-    }
-    return true
-    // const parents = await collectGenerator(getParentNodes(node))
-    // const componentChild = parents.some((parent) => {
-    //     if (isComponentNode(parent)) {
-    //         return true
-    //     }
-    //     return false
-    // })
-    // return !componentChild
-}
-
-const possibleInstanceTextFields = [
-    'text',
-    'placeholder',
-    'label',
-    'title',
-    'description',
-    'hint',
-    'question',
-    'answer',
-    'buttontext',
-    'content',
-]
-
-function nineCharsRandomString() {
-    return Math.random().toString(36).substring(2, 11)
-}
-async function push({
-    node,
-    tree,
-    text,
-    nodeId,
-    controlKey,
-}: {
-    tree: OldTextTree
-    node: AnyNode
-    text?: string
-    nodeId: string
-    controlKey?: string
-}) {
-    // console.trace('push')
-    // console.log(`adding node ${node?.['name']}`)
-    const parents = (await collectGenerator(getParentNodes(node))).reverse()
-    let currentLevel = tree
-
-    // Traverse or create the hierarchy
-    for (let i = 0; i < parents.length; i++) {
-        const parent = parents[i]
-
-        let existingNode = currentLevel.find(
-            (item) => item.nodeId === parent.id,
-        )
-
-        if (!existingNode) {
-            existingNode = {
-                // content: ,
-                nodeId: parent.id,
-                name: supportsName(parent) ? parent.name : '',
-                children: [],
-            }
-            currentLevel.push(existingNode)
-        }
-
-        if (!existingNode.children) {
-            existingNode.children = []
-        }
-
-        currentLevel = existingNode.children
-    }
-
-    let href = undefined as string | undefined
-    if (supportsLink(node)) {
-        href = node.link || undefined
-    }
-    let fontSize
-    if (isTextNode(node)) {
-        fontSize = node.inlineTextStyle?.fontSize || undefined
-    }
-    // Add the actual node
-    currentLevel.push({
-        content: text,
-        nodeId,
-        name: 'name' in node ? node.name : '',
-        attributes: {
-            href,
-            fontSize,
-            controlKey,
-        },
-        children: [],
-    })
-    return tree
-}
-
-async function getFramerTree(rootNodes: AnyNode[]) {
-    let oldText = [] as OldTextTree
-
-    let componentInstanceChildrenSeen = new Set<string>()
-    async function handleNode(node: AnyNode) {
-        // console.log('node', node.constructor.name)
-
-        if (isTextNode(node)) {
-            const isVisible = await isNodeVisible(node)
-            if (!isVisible) {
-                console.log('node not visible', node.id)
-                return
-            }
-            const text = await node.getText()
-
-            if (!text) {
-                console.log('no text found for node', node.id, node.name)
-                return
-            }
-            if (text) {
-                oldText = await push({
-                    node,
-                    tree: oldText,
-                    text,
-                    nodeId: node.id,
-                })
-            }
-        }
-        if (isComponentInstanceNode(node)) {
-            const isVisible = await isNodeVisible(node)
-            if (!isVisible) {
-                console.log('node not visible', node.id)
-                return
-            }
-            // const _component = await getInstanceComponent(node)
-            // if (!_component) {
-            //     return
-            // }
-            const controls = Object.entries(node.controls)
-
-            for (let [key, value] of controls) {
-                if (
-                    typeof value === 'string' &&
-                    // TODO check type when framer supports it
-                    possibleInstanceTextFields.includes(key.toLocaleLowerCase())
-                ) {
-                    let nodeId = nineCharsRandomString()
-                    instanceNodes.set(nodeId, {
-                        node,
-                        controlKey: key,
-                    })
-
-                    oldText = await push({
-                        // parent: node,
-                        controlKey: key,
-                        node,
-                        nodeId,
-                        tree: oldText,
-                        text: value,
-                    })
-                }
-            }
-        }
-    }
-
-    for (let rootNode of rootNodes) {
-        if (!rootNode) {
-            continue
-        }
-
-        for await (let node of rootNode.walk()) {
-            await handleNode(node)
-            for await (let child of recurseIntoComponent(
-                node,
-                componentInstanceChildrenSeen,
-            )) {
-                await handleNode(child)
-            }
-        }
-    }
-
-    oldText = cleanupOldTextTree(oldText)
-    return oldText
-}
-
-async function discardFramerChanges(previousOldText: OldTextTree) {
-    const allNodes = bfsOldTextTree(previousOldText).filter((x) => x?.nodeId)
-    const promises = allNodes.map(async (oldNodeObj) => {
-        const { nodeId, content: oldContent } = oldNodeObj
-        if (!oldContent || !nodeId) {
-            console.log('no old content or node id found')
-            return Promise.resolve()
-        }
-
-        let node =
-            instanceNodes.get(nodeId)?.node || (await framer.getNode(nodeId))
-
-        if (isTextNode(node)) {
-            // console.log('setting text', oldContent)
-            return await node.setText(oldContent)
-        }
-        let instance = instanceNodes.get(nodeId)
-        if (instance) {
-            const { node, controlKey } = instance
-            let controls = { ...node.controls }
-            controls[controlKey] = oldContent
-            return await node.setAttributes({
-                controls,
-            })
-        }
-    })
-    return await Promise.all(promises)
 }
