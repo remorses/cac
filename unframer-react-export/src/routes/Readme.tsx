4a886390b5814b7f80ad1d39794affddca2f1413
remorses <beats.by.morse@gmail.com>
2025-01-17 12:19:33 +0100

    fix react github link

--- unframer-react-export/src/routes/Readme.tsx
+++ unframer-react-export/src/routes/Readme.tsx
@@ -38,7 +38,7 @@ To download and start using your React components, run the following command:
 - Framer variables will be available as **React props**. Breakpoints too.
 
 - These files are **generated**, do not edit them manually. Instead run the command again after changes in Framer.
-- For more info or issues please visit [Unframer GitHub](https://github.com/remorses/unframer).
+- For more info or issues please visit <a href="https://github.com/remorses/unframer" target="_blank" rel="noopener noreferrer">unframer GitHub</a>.
 `
 }
 async function loader({}: LoaderFunctionArgs) {
