8853b5c1a28afd83f28256ab2fc57064ac4cf2ba
remorses <beats.by.morse@gmail.com>
2025-02-21 11:20:12 +0100

    website/src/routes/unframer-open-stackblitz: use button to open stackblitz. fix popup blocked

--- website/src/routes/unframer-open-stackblitz.tsx
+++ website/src/routes/unframer-open-stackblitz.tsx
@@ -1,19 +1,24 @@
 import { Button, Spinner } from '@nextui-org/react'
+import { useSearchParams } from '@remix-run/react'
 import { useEffect } from 'react'
 import { generateStackblitzProject } from 'website/src/lib/utils'
 
 export default function OpenStackblits() {
-    useEffect(() => {
-        const searchParams = new URLSearchParams(window.location.search)
-        const projectId = searchParams.get('projectId')
-        const title = searchParams.get('title') || ''
-        generateStackblitzProject({ projectId, title })
-    }, [])
+    const [searchParams] = useSearchParams()
+    const projectId = searchParams.get('projectId')
+    const title = searchParams.get('title') || ''
+
     return (
         <div className='flex gap-6 flex-col items-center justify-center h-screen'>
-            <Spinner />
-            <Button className=''>
-                <div className='flex items-center text-xs justify-center gap-2'>
+            <h1 className='max-w-sm text-center text-balance'>
+                Click the button below to open a demo project with your Framer components in Stackblitz.
+            </h1>
+            <Button
+                onPress={() => generateStackblitzProject({ projectId, title })}
+                color='primary'
+                className=''
+            >
+                <div className='flex items-center justify-center gap-2'>
                     {/* <MaterialSymbolsBolt className="size-[14px] shrink-0" /> */}
                     Open Demo Project in Stackblitz
                 </div>
