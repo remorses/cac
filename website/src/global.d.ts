e385041571714555fd57a7d05cbad69e312c6db8
remorses <beats.by.morse@gmail.com>
2025-05-20 14:30:33 +0200

    fix tsc issues

--- website/src/global.d.ts
+++ website/src/global.d.ts
@@ -1,4 +1,8 @@
-
-
 // allow importing images
 declare module '*.jpeg'
+
+// https://github.com/remix-run/react-router/issues/12362
+declare module '*/_mdx.guides.migrate-template-affiliate.js' {
+    let MDXComponent: (props: any) => JSX.Element
+    export default MDXComponent
+}
