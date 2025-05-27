0c76f6bf73f2100305f83722376054f276ca4c67
remorses <beats.by.morse@gmail.com>
2025-05-27 10:12:05 +0200

    better no org id error

--- website/src/routes/api.stripe.webhooks.tsx
+++ website/src/routes/api.stripe.webhooks.tsx
@@ -126,7 +126,7 @@ async function handleSubscriptionChange(
 
     if (!orgId) {
         notifyError(
-            new AppError('No orgId in subscription metadata'),
+          new AppError(`No orgId in subscription metadata for subscription ${subscription.id}, customer email: ${subscription.customer}`),
             'Stripe webhook',
         )
         return
