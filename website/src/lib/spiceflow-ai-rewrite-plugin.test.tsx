b7f336ec5dbcbb22b34404325176f097c1a2035f
remorses <beats.by.morse@gmail.com>
2025-06-06 13:54:37 +0200

    added test for the ai rewrite plugin, works with latest model lfg

---
+++ website/src/lib/spiceflow-ai-rewrite-plugin.test.tsx
@@ -0,0 +1,98 @@
+import { createSpiceflowClient } from 'spiceflow/client'
+import { expect, test } from 'vitest'
+import { llmPluginApp } from './spiceflow-ai-rewrite-plugin'
+import {
+    rewriteXmlContentForTests,
+    xmlToOldTextTree
+} from './xml'
+
+test(
+    'ai rewrite can generate',
+    async () => {
+        const orgId = Promise.resolve('7847647d-33a4-466a-8e56-0f03b2dc8ce5')
+        const userId = Promise.resolve('7847647d-33a4-466a-8e56-0f03b2dc8ce5')
+        const client = createSpiceflowClient(llmPluginApp, {
+            state: { orgId, userId },
+        })
+        const xml = `
+    <AI_Kit_Nav>
+   	<Navigation>
+    		<Stack>
+   			<AI_Kit_Navigation_Nav_Top_Item nodeId="w2keh4di">
+    				Features
+   			</AI_Kit_Navigation_Nav_Top_Item>
+   			<AI_Kit_Navigation_Nav_Top_Item nodeId="f9jqiq3c">
+    				Developers
+   			</AI_Kit_Navigation_Nav_Top_Item>
+   			<AI_Kit_Navigation_Nav_Top_Item href="example.com/brand" nodeId="ttyq47m7">
+    				Brand
+   			</AI_Kit_Navigation_Nav_Top_Item>
+   			<AI_Kit_Navigation_Nav_Top_Item href="example.com/blog" nodeId="5mjfkk9h">
+    				Blog
+   			</AI_Kit_Navigation_Nav_Top_Item>
+   			<AI_Kit_Navigation_Nav_Top_Item nodeId="phn7sz8d">
+    				Contact
+   			</AI_Kit_Navigation_Nav_Top_Item>
+    		</Stack>
+    		<Stack>
+   			<AI_Kit_Button nodeId="koeyy0nt">
+    				Sign up for free
+   			</AI_Kit_Button>
+    		</Stack>
+     	</Navigation>
+    </AI_Kit_Nav>
+
+    `
+        const tree = xmlToOldTextTree(xml)
+        const res = await client.llm.generate.post({
+            description:
+                'make it about the website builder called Framer instead',
+            randomId: 'x',
+            projectId:
+                'f667fc580d8d9346cf49d633d75940b95292fa43c24dd7765c9a175ec73f7405',
+            projectName: 'test',
+
+            tree,
+        })
+        expect(res.error).toBeNull()
+        const newContent = [] as any[]
+        for await (const item of res.data || []) {
+            // console.log(item)
+            newContent.push(item)
+        }
+        expect(newContent.length).not.toBe(0)
+        expect(rewriteXmlContentForTests({ xml, newContent }))
+            .toMatchInlineSnapshot(`
+              "
+                  <AI_Kit_Nav>
+                 	<Navigation>
+                  		<Stack>
+                 			<AI_Kit_Navigation_Nav_Top_Item nodeId="w2keh4di">
+                  				Features
+                 			</AI_Kit_Navigation_Nav_Top_Item>
+                 			<AI_Kit_Navigation_Nav_Top_Item nodeId="f9jqiq3c">
+                  				Developers
+                 			</AI_Kit_Navigation_Nav_Top_Item>
+                 			<AI_Kit_Navigation_Nav_Top_Item href="example.com/brand" nodeId="ttyq47m7">
+                  				Brand
+                 			</AI_Kit_Navigation_Nav_Top_Item>
+                 			<AI_Kit_Navigation_Nav_Top_Item href="example.com/blog" nodeId="5mjfkk9h">
+                  				Blog
+                 			</AI_Kit_Navigation_Nav_Top_Item>
+                 			<AI_Kit_Navigation_Nav_Top_Item nodeId="phn7sz8d">
+                  				Contact
+                 			</AI_Kit_Navigation_Nav_Top_Item>
+                  		</Stack>
+                  		<Stack>
+                 			<AI_Kit_Button nodeId="koeyy0nt">
+                  				Sign up for free
+                 			</AI_Kit_Button>
+                  		</Stack>
+                   	</Navigation>
+                  </AI_Kit_Nav>
+
+                  "
+            `)
+    },
+    1000 * 20,
+)
