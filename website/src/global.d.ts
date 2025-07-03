7999d326d9b6ec74cf4c928f8d446ae0885d5a35
remorses <beats.by.morse@gmail.com>
2025-07-03 20:37:14 +0200

    updates, fix tsc

--- website/src/global.d.ts
+++ website/src/global.d.ts
@@ -2,7 +2,7 @@
 declare module '*.jpeg'
 
 // https://github.com/remix-run/react-router/issues/12362
-declare module '*/_mdx.guides.migrate-template-affiliate.js' {
-    let MDXComponent: (props: any) => JSX.Element
-    export default MDXComponent
+declare module '*.mdx' {
+  let MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
 }
