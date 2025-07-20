6c21512649e54bd25a08459ad0fdc3e223ee758f
remorses <beats.by.morse@gmail.com>
2025-07-20 21:13:11 +0200

    type safe href for signout

--- website/src/routes/_board.subscriptions.tsx
+++ website/src/routes/_board.subscriptions.tsx
@@ -2,7 +2,7 @@ import type { LoaderFunctionArgs } from 'react-router'
 
 import { Link } from '@heroui/react'
 import { prisma } from 'db'
-import { data as json, useLoaderData, Form } from 'react-router'
+import { data as json, useLoaderData, Form, href } from 'react-router'
 import Stripe from 'stripe'
 import { env } from '../lib/env'
 import { getSupabaseSession } from '../lib/supabase.server'
@@ -163,7 +163,7 @@ export default function Page() {
                 </div>
             )}
             <div className="mt-8 text-center">
-                <Form method="post" action="/api/auth/signout">
+                <Form method="post" action={href('/api/auth/signout')}>
                     <button
                         type="submit"
                         className="text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-200 underline bg-transparent border-none cursor-pointer"
