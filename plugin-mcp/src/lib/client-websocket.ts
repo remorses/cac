e0282737b0daedae65df8577f29af95da992c223
remorses <beats.by.morse@gmail.com>
2025-07-27 16:41:52 +0200

    adding client websocket handling for mcp

---
+++ plugin-mcp/src/lib/client-websocket.ts
@@ -0,0 +1,67 @@
+import { McpToolWebsocketPayload } from './mcp'
+import { WebsocketMessage } from './websocket'
+
+// Function for handling websocket connection based on session cookie
+export async function websocketClientHandling({
+    handle,
+    websocketId,
+}: {
+    websocketId: string
+    handle: <T extends McpToolWebsocketPayload>(
+        payload: T,
+    ) => Promise<T['output']>
+}) {
+    if (typeof window === 'undefined') return
+    if (globalThis.websocketHandlingDone) return
+    globalThis.websocketHandlingDone = true
+
+    console.log('connecting over preview websocketId', websocketId)
+    const websocketUrl = `wss://fumabase.com/_tunnel/client?id=${websocketId}`
+    const ws = new WebSocket(websocketUrl)
+    ws.onopen = () => {
+        ws.send(JSON.stringify({ type: 'ready' }))
+    }
+    ws.onclose = () => {}
+    ws.onmessage = async (event) => {
+        let data: WebsocketMessage
+        try {
+            data = JSON.parse(event.data)
+        } catch {
+            console.error(`websocket sent invalid json`, event.data)
+            return
+        }
+        const { id, payload } = data || {}
+        if (!payload?.type) {
+            console.error(`websocket sent invalid data`, event.data)
+            return
+        }
+
+        try {
+            const output = await handle(payload)
+            ws.send(
+                JSON.stringify({
+                    id,
+                    payload: {
+                        input: payload.input! as any,
+                        type: payload.type!,
+                        output,
+                    },
+                } satisfies WebsocketMessage),
+            )
+        } catch (e) {
+            console.error(`websocket error`, e)
+            ws.send(
+                JSON.stringify({
+                    id,
+                    error: e instanceof Error ? e.message : String(e),
+                } satisfies WebsocketMessage),
+            )
+        }
+    }
+    // ping interval
+    setInterval(() => {
+        if (ws.readyState === WebSocket.OPEN) {
+            ws.send(JSON.stringify({ type: 'ping' }))
+        }
+    }, 1000)
+}
