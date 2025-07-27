b901934ca87952b9f91e55df0cb066e4800e9615
remorses <beats.by.morse@gmail.com>
2025-07-27 16:08:43 +0200

    pass websocketId to mcp

--- plugin-mcp/src/worker.ts
+++ plugin-mcp/src/worker.ts
@@ -2,28 +2,32 @@ import { Server } from '@modelcontextprotocol/sdk/server/index.js'
 import { McpAgent } from 'agents/mcp'
 import { implementMcpTools } from './lib/mcp'
 
-export class MyMCP extends McpAgent {
+export class MyMCP extends McpAgent<Env> {
     server = new Server({
         name: 'Authless Calculator',
         version: '1.0.0',
     })
     async init() {
         const env = this.env
-
-        await implementMcpTools({ server: this.server })
+        const websocketId = env.websocketId
+        await implementMcpTools({ websocketId, server: this.server })
     }
 }
 
 type Env = {
     MY_MCP: MyMCP
+    websocketId?: string
 }
 
 export default {
     fetch(request: Request, env: Env, ctx: ExecutionContext) {
         const url = new URL(request.url)
-
+        const websocketId = url.searchParams.get('id') as string | undefined
+        env.websocketId = websocketId
         if (url.pathname === '/sse' || url.pathname === '/sse/message') {
-            return MyMCP.serveSSE('/sse').fetch(request, env, ctx)
+            const mcp = MyMCP.serveSSE('/sse')
+
+            return mcp.fetch(request, env, ctx)
         }
 
         if (url.pathname === '/mcp') {
