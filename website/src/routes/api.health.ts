9deb08d9e206f3ae721e8a9d6f82ac4d01a663cb
remorses <beats.by.morse@gmail.com>
2025-09-08 13:08:32 +0200

    nn

--- website/src/routes/api.health.ts
+++ website/src/routes/api.health.ts
@@ -1,34 +1,14 @@
 import { prisma } from 'db'
-async function checkDatabaseConnection() {
-    const randomId = Math.random().toString(36).slice(2, 10)
-    const label = `database-health-check-${randomId}`
-    console.time(label);
-    try {
-        // Perform a lightweight query instead of just connecting
-        await prisma.$queryRaw`SELECT 1;`
-        console.timeEnd(label);
-        return new Response('OK', {
-            status: 200,
-            headers: {
-                'Content-Type': 'text/plain',
-            },
-        })
-    } catch (error) {
-        console.error(error)
-        console.timeEnd(label);
-        return new Response(error?.message, {
-            status: 503,
-            headers: {
-                'Content-Type': 'text/plain',
-            },
-        })
-    }
-}
+import { Route } from './+types/api.health'
+import { createHealthLoader } from '@xmorse/deployment-utils/src/health'
 
-export async function action() {
-    return await checkDatabaseConnection()
+async function check() {
+  const hash = Math.random().toString(36).slice(2, 10)
+  const label = `prisma latency ${hash}`
+  console.time(label)
+  await prisma.$queryRawUnsafe('SELECT 1')
+  console.timeEnd(label)
+  return
 }
-
-export async function loader() {
-    return await checkDatabaseConnection()
-}
+export const loader = createHealthLoader({ check })
+export const action = createHealthLoader({ check })
