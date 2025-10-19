b971fff61f3db6ca6292b99cd0f28ad4f241c337
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-10-19 18:21:57 +0200

    sync schema with supabase

--- db/diff.sql
+++ db/diff.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "public"."ReactExportCreationReason" AS ENUM ('USER_REQUESTED', 'MCP_FIRST_OPEN');
-
 -- AlterTable
-ALTER TABLE "public"."ReactExportProject" ADD COLUMN     "creationReason" "public"."ReactExportCreationReason" NOT NULL DEFAULT 'USER_REQUESTED';
+ALTER TABLE "public"."ReactExportComponentInstance" ADD COLUMN     "styles" JSONB NOT NULL DEFAULT '{}';
 
