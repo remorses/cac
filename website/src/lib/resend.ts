9a90f2282ad43c6ac7001917559c88a680f44072
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-10-21 15:45:03 +0200

    use better unframer email

--- website/src/lib/resend.ts
+++ website/src/lib/resend.ts
@@ -5,5 +5,5 @@ export const resend = new Resend(env.RESEND_API_KEY!)
 
 export const defaultResendOptions = {
   replyTo: 'tommy@unframer.co',
-  from: 'Tommy <tommy@updates.unframer.co>',
+  from: 'Tommy <tommy@tommy.unframer.co>',
 }
